8250f562c380c3f9c65ae84050158b1ef9d70e18
hungqng <hung.qng04@gmail.com>
2023-04-13 00:01:35 -0700

    4-13-2023

---
+++ Combine Two Tables.sql
@@ -0,0 +1,46 @@
+-- 175. Combine Two Tables
+-- Easy
+
+-- 1878
+
+-- 184
+
+-- Add to List
+
+-- Share
+-- SQL Schema
+-- Table: Person
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | personId    | int     |
+-- | lastName    | varchar |
+-- | firstName   | varchar |
+-- +-------------+---------+
+-- personId is the primary key column for this table.
+-- This table contains information about the ID of some persons and their first and last names.
+ 
+
+-- Table: Address
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | addressId   | int     |
+-- | personId    | int     |
+-- | city        | varchar |
+-- | state       | varchar |
+-- +-------------+---------+
+-- addressId is the primary key column for this table.
+-- Each row of this table contains information about the city and state of one person with ID = PersonId.
+ 
+
+-- Write an SQL query to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+-- # Write your MySQL query statement below
+select FirstName, LastName, City, State from Person left join Address on Person.PersonId = Address.PersonId;
