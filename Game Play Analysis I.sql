dbf9578b29553fc5259228136b644bee6834eeaa
hungqng <hung.qng04@gmail.com>
2023-05-09 00:17:10 -0700

    05-09-2023

---
+++ Game Play Analysis I.sql
@@ -0,0 +1,28 @@
+-- 511. Game Play Analysis I
+
+-- SQL Schema
+-- Table: Activity
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | player_id    | int     |
+-- | device_id    | int     |
+-- | event_date   | date    |
+-- | games_played | int     |
+-- +--------------+---------+
+-- (player_id, event_date) is the primary key of this table.
+-- This table shows the activity of players of some games.
+-- Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+ 
+
+-- Write an SQL query to report the first login date for each player.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+-- # Write your MySQL query statement below
+SELECT player_id, min(event_date) as first_login
+FROM Activity
+GROUP BY player_id;
